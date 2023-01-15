5e875adf07a6b65c80878062b8f01bccd8121b25
rakibul-wdp <rakibul.wdp@gmail.com>
2023-01-15 14:34:41 +0600

    create index in services

---
+++ src/app/shared/services/index.ts
@@ -0,0 +1,6 @@
+import { LoaderService } from './loader/loader.service';
+import { UserService } from './user/user.service';
+
+const Services: any = [UserService, LoaderService];
+
+export { Services };
